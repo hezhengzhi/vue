4f0682fb05bf3efd5a238088c1009eb6a25ed5a3
hezhengzhi <460691946@qq.com>
2018-12-04 14:59:04 +0800

    ss

--- vue/src/main/java/com/example/iview/controller/IndexController.java
+++ vue/src/main/java/com/example/iview/controller/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @RequestMapping("/index")
+
+
     public String index(){
         return "index";
     }
