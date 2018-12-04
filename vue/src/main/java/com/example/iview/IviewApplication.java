acbcf61308b3d688c96c19e1e4774c9481741f58
hezhengzhi <460691946@qq.com>
2018-12-04 11:24:29 +0800

    vue

---
+++ vue/src/main/java/com/example/iview/IviewApplication.java
@@ -0,0 +1,12 @@
+package com.example.iview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IviewApplication.class, args);
+    }
+}
---
+++ vue/src/main/java/com/example/iview/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.iview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by pc on 2018/12/3 14:48
+ **/
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
